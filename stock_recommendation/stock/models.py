f19a6ce0bb5b2268d8f521914c84431f04c6ade7
SYNewman <shmuelnewman@hotmail.com>
2024-11-05 17:00:54 +0000

    added model for 'Stock' table in models.py without doing migrations

--- stock_recommendation/stock/models.py
+++ stock_recommendation/stock/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Each class is a model. A model is a table in the database.
+
+# Model for the Stock table
+# Contains basic information about each stock
+class Stock(models.Model):
+    ticker = models.CharField(max_length=5)
+    company_name = models.CharField(max_length=100)
+    sector = models.CharField(max_length=100)
+    last_updated = models.DateTimeField()
