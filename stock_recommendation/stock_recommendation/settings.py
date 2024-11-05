ed60bc0c42115fa17a30ba717ad23d8707ce7047
SYNewman <shmuelnewman@hotmail.com>
2024-11-05 15:25:50 +0000

    Added stock app to installed_apps in settings.py

--- stock_recommendation/stock_recommendation/settings.py
+++ stock_recommendation/stock_recommendation/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stock', # This is my app (for the main part of the application)
 ]
 
 MIDDLEWARE = [
