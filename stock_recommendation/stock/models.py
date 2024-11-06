4d899ce48550cade2fd724a9a44bb0a47c54acfb
SYNewman <shmuelnewman@hotmail.com>
2024-11-06 19:24:17 +0000

    reordered models

--- stock_recommendation/stock/models.py
+++ stock_recommendation/stock/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Each class is a model. A model is a table in the database.
 
+#Model for the Users table
+#Contains information about the users
+class Users(models.Model):
+    pass
+
 # Model for the Stock table
 # Contains basic information about each stock
 class Stock(models.Model):
@@ -30,8 +35,3 @@ class Recommendations(models.Model):
     stock_id = models.ForeignKey(Stock, on_delete=models.CASCADE)
     recommendation_type = models.CharField(max_length=100)
     generated_time = models.DateTimeField()
-
-#Model for the Users table
-#Contains information about the users
-class Users(models.Model):
-    pass
