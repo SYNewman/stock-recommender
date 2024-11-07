c17f6842435720d5dcd26ea2a5eaa55936217153
SYNewman <shmuelnewman@hotmail.com>
2024-11-07 22:25:07 +0000

    made template for stock_list.py

---
+++ stock_recommendation/stock/app_logic/stock_lists.py
@@ -0,0 +1,23 @@
+'''
+This file consists of only lists
+Each list represents an ETF or sector
+Each item in a list is the name of a stock
+'''
+
+'''
+This is the list of ETF's and sectors in this file
+in the order they appear in this file for easy reference:
+ - ETF_LIST
+ - SPY
+ - QQQ
+ - VOO
+'''
+
+ETF_LIST = ['SPY', 'QQQ', 'VOO']
+
+SPY = [] # Put all stocks in the SPY ETF in this list
+
+QQQ = [] # Put all stocks in the QQQ ETF in this list
+
+VOO = [] # Put all stocks in the VOO ETF in this list
+
