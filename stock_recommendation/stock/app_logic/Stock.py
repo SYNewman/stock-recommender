f3d607e405b4a5208206845e945b83a31ffd4585
SYNewman <shmuelnewman@hotmail.com>
2024-11-17 14:47:49 +0000

    In Stock class added attributes to __init__ & get_data func

--- stock_recommendation/stock/app_logic/Stock.py
+++ stock_recommendation/stock/app_logic/Stock.py
@@ -1,3 +1,11 @@
 class Stock:
-    def __init__():
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.price_history = None
+        self.signals = {}
+        self.recommendation = None
+        
+    def get_data(self):
         pass
+    
+    
