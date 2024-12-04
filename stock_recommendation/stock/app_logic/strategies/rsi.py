31ec41103b2f558052848b13321bd59b91c3f431
SYNewman <shmuelnewman@hotmail.com>
2024-12-04 16:58:19 +0000

    Updated all methods to put vars in the constructor

--- stock_recommendation/stock/app_logic/strategies/rsi.py
+++ stock_recommendation/stock/app_logic/strategies/rsi.py
@@ -2,77 +2,76 @@ from Strategies import Strategy
 
 class RSI_Strategy(Strategy):
     
-    def __init__(self, strategy, stats):
+    def __init__(self, strategy, stats, price):
         super().__init__(strategy, stats)
         self.time_period = 14
         self.stats = stats
+        self.price = price
+        self.price_changes = []
+        self.gains = []
+        self.losses = []
+        self.average_gain = 0
+        self.average_loss = 0
+        self.second_average_gain = 0
+        self.second_average_loss = 0
+        self.rs_value = 0
+        self.rsi_value = 0
     
     def calculate_daily_price_change(self):
-        all_changes = []
         for i in range(len(self.stats-1)):
             price_change = self.stats[i]['price'] - self.stats[i+1]['price']
-            all_changes.append(price_change)
-        return all_changes
+            self.price_changes.append(price_change)
     
-    def separate_gains_and_losses(self, changes):
-        gains = []
-        losses = []
-        for i in changes:
+    def separate_gains_and_losses(self):
+        for i in self.price_changes:
             if i > 0:
-                gains.append(i)
-                losses.append(0)
+                self.gains.append(i)
+                self.losses.append(0)
             elif i < 0:
-                gains.append(0)
-                losses.append(i*-1)
+                self.gains.append(0)
+                self.losses.append(i*-1)
             else:
-                gains.append(0)
-                losses.append(0)
-        return gains, losses
+                self.gains.append(0)
+                self.losses.append(0)
     
-    def calculate_average_gain(self, gains): # This and next method - maybe put into one smaller method
+    def calculate_average_gain(self): # This and next method - maybe put into one smaller method
         total_gain = 0
-        for i in gains:
+        for i in self.gains:
             total_gain += i
-        average_gain = total_gain / self.time_period
-        return average_gain
+        self.average_gain = total_gain / self.time_period
     
-    def calculate_average_loss(self, losses):
+    def calculate_average_loss(self):
         total_loss = 0
-        for i in losses:
+        for i in self.losses:
             total_loss += i
-        average_loss = total_loss / self.time_period
-        return average_loss
+        self.average_loss = total_loss / self.time_period
     
-    def calculate_second_average_gain(self, average_gain): # To smooth out the values
-        current_gain = price - self.stats[i['price']]
+    def calculate_second_average_gain(self): # To smooth out the values
+        current_gain = self.price - self.stats[i['price']]
         if current_gain < 0:
             current_gain = 0
         previous_average_gain = average_gain * (self.time_period - 1)
-        second_average_gain = (previous_average_gain + current_gain) / self.time_period
-        return second_average_gain
+        self.second_average_gain = (previous_average_gain + current_gain) / self.time_period
         
-    def calculate_second_average_loss(self, average_loss): # To smooth out the values
-        current_loss = price - self.stats[i['price']] # put real price (at runtime) here
+    def calculate_second_average_loss(self): # To smooth out the values
+        current_loss = self.price - self.stats[i['price']] # put real price (at runtime) here
         if current_loss < 0:
             current_loss = 0
         previous_average_loss = average_loss * (self.time_period - 1)
-        second_average_loss = (previous_average_loss + current_loss) / self.time_period
-        return second_average_loss
+        self.second_average_loss = (previous_average_loss + current_loss) / self.time_period
     
-    def calculate_relative_strength(self, second_average_gain, second_average_loss):
-        if second_average_loss == 0:
-            return float('inf')
-        rs = second_average_gain / second_average_loss
-        return rs
+    def calculate_relative_strength(self):
+        if self.second_average_loss == 0:
+            self.rs_value = float('inf')
+        self.rs_value = self.second_average_gain / self.second_average_loss
     
-    def calculate_rsi_value(self, rs):
-        rsi = 100 - (100 / (rs+1))
-        return rsi
+    def calculate_rsi_value(self):
+        self.rsi_value = 100 - (100 / (self.rs+1))
     
-    def apply_strategy(self, stock, rsi):
-        if rsi < 30:
+    def apply_strategy(self):
+        if self.rsi_value < 30:
             #buy
-        elif rsi > 70:
+        elif self.rsi_value > 70:
             #sell
         else:
             #hold
