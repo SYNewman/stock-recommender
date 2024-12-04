be5ba04f4df70b3ddbca7d6bed5601b6777a22ca
SYNewman <shmuelnewman@hotmail.com>
2024-12-04 19:20:25 +0000

    Put all vars in the constructor

--- stock_recommendation/stock/app_logic/strategies/moving_averages.py
+++ stock_recommendation/stock/app_logic/strategies/moving_averages.py
@@ -2,29 +2,29 @@ from Strategies import Strategy
 
 class Moving_Average_Strategy(Strategy):
     
-    def __init__(self, strategy, stats):
+    def __init__(self, strategy, stats, price):
         super().__init__(strategy, stats)
         self.short_term_moving_average = 0
         self.long_term_moving_average = 0
+        self.stats = stats
+        self.price = price
         
     def calculate_short_term_moving_average(self):
         total_close_prices = 0
-        for i in (close price for each of last 50 trading days):
+        for i in (close price for each of last 50 trading days): # should be first 50 days of stats
             total_close_prices += i
         self.short_term_moving_average = total_close_prices/50
-        return self.short_term_moving_average
         
     def calculate_long_term_moving_average(self):
         total_close_prices = 0
-        for i in (close price for each of last 200 trading days):
+        for i in self.stats:
             total_close_prices += i
         self.long_term_moving_average = total_close_prices/200
-        return self.long_term_moving_average
     
-    def apply_strategy(self, stock): #price var needs to be passed or defined
-        if price > self.short_term_moving_average and self.short_term_moving_average > self.long_term_moving_average:
+    def apply_strategy(self):
+        if self.price > self.short_term_moving_average and self.short_term_moving_average > self.long_term_moving_average:
             #buy
-        elif price < self.short_term_moving_average and self.short_term_moving_average < self.long_term_moving_average:
+        elif self.price < self.short_term_moving_average and self.short_term_moving_average < self.long_term_moving_average:
             #sell
         else:
             #hold
