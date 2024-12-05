70ee97f8fa318f34fa7bc5045b137dae6b035e12
SYNewman <shmuelnewman@hotmail.com>
2024-12-05 14:21:54 +0000

    Updated import statement

--- stock_recommendation/stock/views.py
+++ stock_recommendation/stock/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Recommendations
 from .models import Stock
-from stock_recommendation.app_logic import Trading_System
+from .app_logic import Trading_System
 
 # Create your views here.
 def home_page(request):
