eebba878f16f2080937fd196fffc7f35b493164c
SYNewman <shmuelnewman@hotmail.com>
2024-12-30 21:03:10 +0000

    Added URL for options calculator

--- stock_recommendation/stock/urls.py
+++ stock_recommendation/stock/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name="home_page"),
     path('recommendations', views.recommendations_page, name='recommendations'),
+    path('options-pricing-calculator', views.options_calculator_page, name='options-pricing-calculator'),
     path('sign_up', views.sign_up_page, name="sign_up_page"),
     path('log_in', views.log_in_page, name="log_in_page"),
 ]
