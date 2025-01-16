c6d978bc468223b199c02ec9ef3f5fb1a8d2abf5
SYNewman <shmuelnewman@hotmail.com>
2025-01-16 21:20:35 +0000

    Added models to allow me to see them in admin

--- stock_recommendation/stock/admin.py
+++ stock_recommendation/stock/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Stock)
+admin.site.register(StockData)
+admin.site.register(Strategies)
+admin.site.register(Recommendations)
